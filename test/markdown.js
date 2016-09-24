21bc733788604ae443fc5c42a44a12f376aa09c6
bredele <olivier.wietrich@gmail.com>
2016-09-24 11:59:54 -0600

    render script

--- test/markdown.js
+++ test/markdown.js
@@ -18,7 +18,7 @@ test('should render markdown', assert => {
 
 test('should render vomit snippet', assert => {
   assert.plan(1)
-  var result = concat(data => assert.equal(data.toString(), '<h1 id="hello">hello</h1>\n'))
+  var result = concat(data => assert.equal(data.toString(), '<script>\n    var fn = (function() {function component(data) {\n  return vomit`<button>${data}</button>`\n}return component})()\n    var el = fn("hello")\n    document.currentScript.parentElement.appendChild(el)\n    </script>'))
   fs.createReadStream(__dirname + '/snippet.md')
     .pipe(markdown({
       data: 'hello'
